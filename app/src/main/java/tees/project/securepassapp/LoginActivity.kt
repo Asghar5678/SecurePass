779c9595b6f2016c3349c43e9a872626c4b7bfc5
Asghar5678 <S3345558@live.tees.ac.uk>
2025-11-15 20:36:17 +0530

    Designed Splash Screen with label and logo

---
+++ app/src/main/java/tees/project/securepassapp/LoginActivity.kt
@@ -0,0 +1,14 @@
+package tees.project.securepassapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
